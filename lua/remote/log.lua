789376e2860c03a124aff87b919b7a94ede44558
niuiic <niuiicde@gmail.com>
2023-10-11 10:59:44 +0800

    perf(log): add level option

--- lua/remote/log.lua
+++ lua/remote/log.lua
@@ -2,11 +2,12 @@ local static = require("remote.static")
 
 --- write log
 ---@param log string
-local write_log = function(log)
+---@param level "ERROR" | "WARN" | "INFO"
+local write_log = function(log, level)
 	if not static.config.log_file then
 		return
 	end
-	vim.fn.writefile({ log }, static.config.log_file, "a")
+	vim.fn.writefile({ string.format("[%s]: %s", level, log) }, static.config.log_file, "a")
 end
 
 return { write_log = write_log }
