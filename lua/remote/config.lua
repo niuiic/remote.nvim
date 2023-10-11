ac8c3d4fbab32c22a3b2b9795ae9408bc09c717f
niuiic <niuiicde@gmail.com>
2023-10-11 10:57:52 +0800

    fix(config): `excludes` type error

--- lua/remote/config.lua
+++ lua/remote/config.lua
@@ -1,34 +1,32 @@
 --- collect config with user input
 ---@param config Remote.ConfigPartial | nil
 local collect_config = function(config)
+	local core = require("core")
+
 	config = config or {}
-	if not config.host then
-		vim.ui.input({ title = "Remote", prompt = "Host: " }, function(input)
-			config.host = input
-		end)
-	end
 
-	if not config.user then
-		vim.ui.input({ title = "Remote", prompt = "User: " }, function(input)
-			config.user = input
-		end)
-	end
-
-	if not config.passwd then
-		vim.ui.input({ title = "Remote", prompt = "Password: " }, function(input)
-			config.passwd = input
-		end)
-	end
-
-	if not config.path then
-		vim.ui.input({ title = "Remote", prompt = "Target path: " }, function(input)
-			config.path = input
-		end)
+	for _, field in ipairs({
+		"host",
+		"user",
+		"passwd",
+		"path",
+	}) do
+		if not config[field] then
+			vim.ui.input(
+				{ title = "Remote", prompt = string.upper(string.sub(field, 1, 2)) .. string.sub(field, 2) .. " :" },
+				function(input)
+					config[field] = input
+				end
+			)
+		end
 	end
 
 	if not config.excludes then
 		vim.ui.input({ title = "Remote", prompt = "Files be excluded(split with comma): " }, function(input)
-			config.excludes = input
+			if not input then
+				return
+			end
+			config.excludes = core.lua.string.split(",")
 		end)
 	end
 
@@ -39,16 +37,15 @@ end
 ---@param config Remote.Config
 ---@return boolean
 local validate_config = function(config)
-	if not config.host then
-		return false
-	end
-
-	if not config.user then
-		return false
-	end
-
-	if not config.path then
-		return false
+	for _, field in ipairs({
+		"host",
+		"user",
+		"passwd",
+		"path",
+	}) do
+		if not config[field] then
+			return false
+		end
 	end
 
 	return true
